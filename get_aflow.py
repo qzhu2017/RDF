189d33fb1513d36fc6aa527a6104e079bee66b6d
Qiang Zhu <alecfans@gmail.com>
2018-04-19 15:07:36 -0700

    modify the script for ML with Aflowlib

--- get_aflow.py
+++ get_aflow.py
@@ -7,35 +7,15 @@ from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 results = search(batch_size=20
         ).filter(K.Egap > 1
         ).filter(K.nspecies == 2
-        ).filter(K.natoms == 2)
+        ).filter(K.natoms < 5)
+print(len(results))
 
+X = []
+Y = []
 for result in results:
-    para = result.geometry
-    para[3:] = para[3:]*pi/180
-    lattice = crystal.para2matrix(para)
-    atom_type = result.species
-    composition = result.composition
-    coordinate = result.positions_fractional
-    site = []
-    
-    for numIon, ele in zip(composition, atom_type):
-        ele = ele.replace('\n','')
-        for x in range(numIon):
-            site.append(ele)
-   
-    struc = Structure(lattice, site, coordinate)
-    finder = SpacegroupAnalyzer(struc, symprec=0.06, angle_tolerance=5)
-    struc = finder.get_conventional_standard_structure()
-    composition=[]
-    dict = struc.composition.as_dict()
-    for ele in struc.composition.elements:
-        composition.append(int(dict[ele.name]))
+    crystal = Structure.from_str(result.files['CONTCAR.relax.vasp'](), fmt='poscar')
+    X.append(RDF(crystal).RDF[:,1])
+    Y.append(result.Egap)
 
-    crystal0 = crystal(fileformat='poscar', 
-        lattice= struc.lattice.matrix,
-        atom_type = atom_type,
-        composition = composition,
-        coordinate = struc.frac_coords)
-    rdf = RDF(crystal0).RDF
-    print(rdf)  # this is X
-    print(result.Egap) #this is Y
+#Store this file
+#Meachine Learning
